ef9222826b2631ec9c3c5e5f5f0141012816b95c
Soo Yang, Tan <sooyang.code@gmail.com>
2018-03-16 00:08:53 +0800

    Refactor inventories controller

--- app/controllers/api/inventories_controller.rb
+++ app/controllers/api/inventories_controller.rb
@@ -2,6 +2,7 @@ module Api
   # Controller for Inventories
   class InventoriesController < ApplicationController
     include JsonErrors
+    include ObjectFinder
 
     before_action :find_inventory, only: %i[addition reduction reserve]
     before_action :distribution_centers, only: %i[current_stock_on_hand current_pending_shipped]
@@ -19,8 +20,10 @@ module Api
 
     def reduction
       if @inventory
-        reduce_inventory
-        reduce_inventory_response
+        inventory_shipped = InventoryMovement.new(
+          @inventory, inventory_params[:quantity].to_i
+        ).shipped
+        inventory_response(inventory_shipped)
       else
         error
       end
@@ -28,8 +31,10 @@ module Api
 
     def reserve
       if @inventory
-        reserve_inventory
-        reserve_inventory_response
+        inventory_reserved = InventoryMovement.new(
+          @inventory, inventory_params[:quantity].to_i
+        ).reserve
+        inventory_response(inventory_reserved)
       else
         error
       end
@@ -45,57 +50,13 @@ module Api
 
     private
 
-    def reduce_inventory
-      @inventory_shipped = InventoryMovement.new( @inventory, inventory_params[:quantity].to_i ).shipped
-    end
-
-    def reduce_inventory_response
-      if @inventory_shipped.errors.present?
-        render json: { errors: @inventory_shipped.errors.full_messages },
+    def inventory_response(inventory)
+      if inventory.errors.present?
+        render json: { errors: inventory.errors.full_messages },
                status: :bad_request
       else
         render template: 'api/inventories/inventory', status: :ok
       end
-    end
-
-    def reserve_inventory
-      @inventory_reserved = InventoryMovement.new(
-        @inventory, inventory_params[:quantity].to_i
-      ).reserve
-    end
-
-    def reserve_inventory_response
-      if @inventory_reserved.errors.present?
-        render json: { errors: @inventory_reserved.errors.full_messages },
-               status: :bad_request
-      else
-        render template: 'api/inventories/inventory', status: :ok
-      end
-    end
-
-    def distribution_center
-      @distribution_center =
-        DistributionCenter.find_by(
-          country: inventory_params[:distribution_center]
-        )
-    end
-
-    def distribution_centers
-      @distribution_centers =
-        DistributionCenter.all.includes(:inventories, inventories: :stock_keeping_unit)
-    end
-
-    def stock_keeping_unit
-      @stock_keeping_unit =
-        StockKeepingUnit.find_by(code: inventory_params[:stock_keeping_unit])
-    end
-
-    def find_inventory
-      return unless stock_keeping_unit && distribution_center
-      @inventory = Inventory.find_or_create_by(
-        distribution_center: distribution_center,
-        stock_keeping_unit: stock_keeping_unit
-      )
     end
 
     def inventory_params
---
+++ app/controllers/concerns/object_finder.rb
@@ -0,0 +1,27 @@
+# Module for providing finder helpers
+module ObjectFinder
+  def distribution_center
+    @distribution_center =
+      DistributionCenter.find_by(
+        country: inventory_params[:distribution_center]
+      )
+  end
+
+  def distribution_centers
+    @distribution_centers =
+      DistributionCenter.all.includes(:inventories, inventories: :stock_keeping_unit)
+  end
+
+  def stock_keeping_unit
+    @stock_keeping_unit =
+      StockKeepingUnit.find_by(code: inventory_params[:stock_keeping_unit])
+  end
+
+  def find_inventory
+    return unless stock_keeping_unit && distribution_center
+    @inventory = Inventory.find_or_create_by(
+      distribution_center: distribution_center,
+      stock_keeping_unit: stock_keeping_unit
+    )
+  end
+end
